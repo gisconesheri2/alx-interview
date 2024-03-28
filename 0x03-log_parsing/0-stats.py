4d4f703664951e20e0858d25b7f1fa7284df8606
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-28 10:36:42 +0300

    parse logs

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Parse logging files
+"""
+import signal
+import sys
+import re
+
+
+def handler(signum, frame):
+    """Handler for a SIGINT (ctrl-c) signal
+    """
+    print('File size: {}'.format(file_size))
+    codes_sorted = sorted(codes.keys(), key=lambda k: int(k))
+    for cd in codes_sorted:
+        print('{}: {}'.format(cd, codes.get(cd)))
+
+
+signal.signal(signal.SIGINT, handler)
+
+file_size = 0
+codes = {}
+count = 0
+# matches 117.254.206.229 - [2024-03-28 10:10:52.224640]\
+# "GET /projects/260 HTTP/1.1" 405 829
+# and groups the last two numbers
+p = r'^[\d+.]*\s-\s\[[\d+-:.\s]*\]\s[\\"\w.\s/]*\s(\d+)\s(\d+)'
+pattern = re.compile(p)
+
+#readline from stdout
+line = sys.stdin.readline()
+while line:
+    if count % 10 == 0 and count != 0:
+        print('File size: {}'.format(file_size))
+        codes_sorted = sorted(codes.keys(), key=lambda k: int(k))
+        for cd in codes_sorted:
+            print('{}: {}'.format(cd, codes.get(cd)))
+    try:
+        code, size = pattern.search(line).groups()
+        codes[code] = codes.get(code, 0) + 1
+        file_size += int(size)
+    except Exception:
+        pass
+    
+    line = sys.stdin.readline()
+    count += 1
