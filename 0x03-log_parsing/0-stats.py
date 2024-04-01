a9c272aa8862c3fcd93adf2b12a9aadbcb89f00d
gisconesheri2 <pgikonyo2022@gmail.com>
2024-04-01 18:54:48 +0300

    make file executable

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,7 +9,7 @@ import re
 
 def handler(signum, frame):
     """
-    Handler for a SIGINT (ctrl-c) signal
+    Handler for a SIGINT (ctrl-c) signals
     """
     print('File size: {:d}'.format(file_size))
     codes_sorted = sorted(codes.keys(), key=lambda k: int(k))
