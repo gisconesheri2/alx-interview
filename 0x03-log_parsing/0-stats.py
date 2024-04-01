bd16a0468ace3ca66ee96fb4f80358b83cf9e7ce
gisconesheri2 <pgikonyo2022@gmail.com>
2024-04-01 19:46:14 +0300

    change exception

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,17 +8,17 @@ import sys
 import re
 
 
-def handler(signum, frame):
-    """
-    Handler for a SIGINT (ctrl-c) signal
-    """
-    print('File size: {:d}'.format(file_size))
-    codes_sorted = sorted(codes.keys(), key=lambda k: int(k))
-    for cd in codes_sorted:
-        print('{}: {:d}'.format(cd, codes.get(cd)))
+# def handler(signum, frame):
+#    """
+#    Handler for a SIGINT (ctrl-c) signal
+#    """
+#    print('File size: {:d}'.format(file_size))
+#    codes_sorted = sorted(codes.keys(), key=lambda k: int(k))
+#    for cd in codes_sorted:
+#        print('{}: {:d}'.format(cd, codes.get(cd)))
 
 
-signal.signal(signal.SIGINT, handler)
+#signal.signal(signal.SIGINT, handler)
 
 file_size = 0
 codes = {}
@@ -29,18 +29,24 @@ count = 0
 p = r'^[\d+.]*\s-\s\[[\d+-:.\s]*\]\s[\\"\w.\s/]*\s(\d+)\s(\d+)'
 pattern = re.compile(p)
 # readline from stdout
+try:
+    for line in sys.stdin:
+        if count % 10 == 0 and count != 0:
+            print('File size: {:d}'.format(file_size))
+            codes_sorted = sorted(codes.keys(), key=lambda k: int(k))
+            for cd in codes_sorted:
+                print('{}: {:d}'.format(cd, codes.get(cd)))
+        try:
+            code, size = pattern.search(line).groups()
+            codes[code] = codes.get(code, 0) + 1
+            file_size += int(size)
+        except Exception:
+            pass
 
-for line in sys.stdin:
-    if count % 10 == 0 and count != 0:
-        print('File size: {:d}'.format(file_size))
-        codes_sorted = sorted(codes.keys(), key=lambda k: int(k))
-        for cd in codes_sorted:
-            print('{}: {:d}'.format(cd, codes.get(cd)))
-    try:
-        code, size = pattern.search(line).groups()
-        codes[code] = codes.get(code, 0) + 1
-        file_size += int(size)
-    except Exception:
-        pass
-
-    count += 1
+        count += 1
+except KeyboardInterrupt:
+    print('File size: {:d}'.format(file_size))
+    codes_sorted = sorted(codes.keys(), key=lambda k: int(k))
+    for cd in codes_sorted:
+        print('{}: {:d}'.format(cd, codes.get(cd)))
+    raise
