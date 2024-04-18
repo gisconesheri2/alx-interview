ad4567b17e46ab5982240fec8981ba17ec3b01e7
gisconesheri2 <pgikonyo2022@gmail.com>
2024-04-18 22:58:51 +0300

    js star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,31 @@
 #!/usr/bin/node
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  throw new Error('need one argument');
-}
+if (process.argv.length > 2) {
+  const num = process.argv[2];
+  const url = `https://swapi-api.alx-tools.com/api/films/${num}/`;
 
-const num = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${num}/`;
-
-function getCharacter (url) {
-  return new Promise((resolve, reject) => {
-    request(url, function (error, response, body) {
-      if (error) {
-        reject(error);
-      }
-      resolve((JSON.parse(body).name));
+  function getCharacter (url) {
+    return new Promise((resolve, reject) => {
+      request(url, function (error, response, body) {
+        if (error) {
+          reject(error);
+        }
+        resolve((JSON.parse(body).name));
+      });
     });
+  }
+
+  request(url, function (error, responce, body) {
+    if (error) {
+      console.log(error);
+    }
+    const jsonBody = JSON.parse(body);
+    const charactersUrls = jsonBody.characters;
+    const charactersName = charactersUrls.map(getCharacter);
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
-
-request(url, function (error, responce, body) {
-  if (error) {
-    console.log(error);
-  }
-  const jsonBody = JSON.parse(body);
-  const charactersUrls = jsonBody.characters;
-  const charactersName = charactersUrls.map(getCharacter);
-
-  Promise.all(charactersName)
-    .then(names => console.log(names.join('\n')))
-    .catch(allErr => console.log(allErr));
-});
